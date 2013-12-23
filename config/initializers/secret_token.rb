246423d1b61d4457c7c826900fcfe87875d46069
Matteo Depalo <matteodepalo@gmail.com>
2013-12-23 17:15:20 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Notdvs::Application.config.secret_key_base = '313cf4f430ef7082b9fbe0ff8ea597eb8ccc8d2a35f301a3b0c8ff6a6a7e9b994b75f1cafa6b6e467fe9ef301d648ad6a93bbb7f0a2381ca2ce4d1212585274c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Notdvs::Application.config.session_store :cookie_store, key: '_notdvs_session'
