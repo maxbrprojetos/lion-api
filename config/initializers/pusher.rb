93dd79c4d3198f661c64c2bbe49153c8eb264837
Matteo Depalo <matteodepalo@gmail.com>
2013-12-31 10:08:43 +0100

    Extract PUSHER_KEY from PUSHER_URL for production

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -2,6 +2,8 @@ if ENV['PUSHER_APP_ID']
   Pusher.app_id = ENV['PUSHER_APP_ID']
   Pusher.key = ENV['PUSHER_KEY']
   Pusher.secret = ENV['PUSHER_SECRET']
+elsif ENV['PUSHER_URL']
+  ENV['PUSHER_KEY'] = ENV['PUSHER_URL'].match(/https?:\/\/(.*):.*/)[1]
 end
 
 Pusher.encrypted = true
