cd53bdb21f0089ec9472642056142201c6d250b4
Matteo Depalo <matteodepalo@gmail.com>
2013-12-31 16:50:31 +0100

    Refactor pusher initializer

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,9 +1,9 @@
-if ENV['PUSHER_APP_ID']
+ENV['PUSHER_KEY'] = ENV['PUSHER_URL'].match(/https?:\/\/(.*):.*/)[1] if ENV['PUSHER_URL']
+
+if ENV['PUSHER_APP_ID'] && ENV['PUSHER_KEY'] && ENV['PUSHER_SECRET']
   Pusher.app_id = ENV['PUSHER_APP_ID']
   Pusher.key = ENV['PUSHER_KEY']
   Pusher.secret = ENV['PUSHER_SECRET']
-elsif ENV['PUSHER_URL']
-  ENV['PUSHER_KEY'] = ENV['PUSHER_URL'].match(/https?:\/\/(.*):.*/)[1]
 end
 
 Pusher.encrypted = true
