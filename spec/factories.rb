03e5c51b743eb7f03dea2f594db92a7385106bb5
Matteo Depalo <matteodepalo@gmail.com>
2014-03-11 13:40:37 +0000

    Revert "Make email not mandatory"
    This reverts commit 82f9a02c3d8b800f764a2f664a5481e834476913.

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
     sequence(:name) { |n| "Test#{n}" }
     sequence(:nickname) { |n| "test#{n}" }
     sequence(:avatar_url) { |n| "http://lol.com/omg#{n}.png" }
+    sequence(:email) { |n| "test#{n}@test.com" }
     sequence(:api_token) { |n| "123456789#{n}" }
     sequence(:github_id) { |n| n }
   end
