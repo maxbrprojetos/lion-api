239bb2adb7730571f9cccb1378ef3a29c421b0a5
Matteo Depalo <matteodepalo@gmail.com>
2014-03-17 14:27:53 +0000

    Really use the payload

--- app/controllers/api/pull_requests_controller.rb
+++ app/controllers/api/pull_requests_controller.rb
@@ -1,7 +1,26 @@
 class Api::PullRequestsController < ApplicationController
   def create
-    PullRequest.create(data: params['payload']['pull_request']) if params['payload']['action'] == 'closed'
+    PullRequest.create(data: pull_request_params) if action == 'closed'
 
     head :ok
+  end
+
+  private
+
+  def pull_request_params
+    if params['payload']
+      params['payload']['pull_request']
+    else
+      params['pull_request']
+    end
+  end
+
+  def action
+    # the magic params[:action] masks the github param
+    if params['payload']
+      params['payload']['action']
+    else
+      JSON.parse(request.body.read)['action']
+    end
   end
 end
