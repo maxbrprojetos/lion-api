1d979f1b0754a5ece292d63a230569439d91614f
Matteo Depalo <matteodepalo@gmail.com>
2014-03-19 20:18:59 +0000

    Use the right scoring time for pull request reviews

--- app/models/pull_request_review.rb
+++ app/models/pull_request_review.rb
@@ -28,4 +28,8 @@ class PullRequestReview < ActiveRecord::Base
   def body_must_contain_positive_signs
     errors.add(:body, 'must contain positive signs') unless body.match(/:\+1:/)
   end
+
+  def scoring_time
+    pull_request.merged_at
+  end
 end
