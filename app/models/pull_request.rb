77a5050de1ad7d0e22a1e1c868bfb008e4da30ef
Matteo Depalo <matteodepalo@gmail.com>
2014-04-01 16:15:22 -0400

    Give 100 points for big deletions

--- app/models/pull_request.rb
+++ app/models/pull_request.rb
@@ -53,7 +53,9 @@ class PullRequest < ActiveRecord::Base
   end
 
   def points
-    if number_of_additions > 500
+    if number_of_deletions > 2 * number_of_additions && number_of_deletions > 1000
+      100
+    elsif number_of_additions > 500
       50
     elsif number_of_deletions > 2 * number_of_additions && number_of_deletions > 100
       30
