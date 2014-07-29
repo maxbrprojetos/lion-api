9e0635c8d1a658622fc044b4e204d12f0928e80e
Eamon OBrien <eamon.c.obrien@gmail.com>
2014-07-29 12:21:28 -0400

    Remove created at and updated at lines

--- db/migrate/20140929114547_create_badges.rb
+++ db/migrate/20140929114547_create_badges.rb
@@ -4,8 +4,6 @@ class CreateBadges < ActiveRecord::Migration
       t.uuid :user_id
       t.uuid :pull_request_id
       t.text :body
-      t.datetime :created_at
-      t.datetime :updated_at
 
       t.timestamps
     end
