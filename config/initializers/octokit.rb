6f64cd48ba9d2ba7bab163f245805974c5039e87
Matteo Depalo <matteodepalo@gmail.com>
2015-03-19 10:19:40 +0000

    Check if primary user exists

--- config/initializers/octokit.rb
+++ config/initializers/octokit.rb
@@ -1,4 +1,4 @@
-if ActiveRecord::Base.connection.table_exists?('users')
+if ActiveRecord::Base.connection.table_exists?('users') && User.find_by(nickname: ENV['PRIMARY_USER_NICKNAME'])
   $primary_user_client = User.find_by(nickname: ENV['PRIMARY_USER_NICKNAME']).github_client
   $primary_user_client.auto_paginate = true
 end
