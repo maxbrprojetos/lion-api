bcc9b9b1abc0cb2da6c10755c724ec319c3edc7c
Matteo Depalo <matteodepalo@gmail.com>
2015-03-19 10:15:31 +0000

    Check if users table exists before running initializer

--- config/initializers/octokit.rb
+++ config/initializers/octokit.rb
@@ -1,2 +1,4 @@
-$primary_user_client = User.find_by(nickname: ENV['PRIMARY_USER_NICKNAME']).github_client
-$primary_user_client.auto_paginate = true
+if ActiveRecord::Base.connection.table_exists?('users')
+  $primary_user_client = User.find_by(nickname: ENV['PRIMARY_USER_NICKNAME']).github_client
+  $primary_user_client.auto_paginate = true
+end
