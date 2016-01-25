e1c7dec1305d29d25cfae432f039611a127e2ddf
Kaosisochukwu Uzokwe <kuzokwe@gmail.com>
2016-01-25 13:30:45 -0500

    Refactor /api/stats endpoint to receive category parameter
    What's up
    Currently, the `/api/stats` endpoint returns all stats under encapsulated in a user object and the payload looks thus:
    
    ```json
    stats: [
      {
        "id": "1",
        "avatar_url": "http://www.example.org/picture1.jpg",
        "nickname": "octocat1",
        "pullRequestCount": 112,
        "pullRequestReviewsCount": 5,
        "additionsCount": 1223,
        "deletionsCount": 2342,
        "badgesCount": 3
      },
      {
        "id": "2",
        "avatar_url": "http://www.example.org/picture2.jpg",
        "nickname": "octocat2",
        "pullRequestCount": 467,
        "pullRequestReviewsCount": 122,
        "additionsCount": 51223,
        "deletionsCount": 24342,
        "badgesCount": 23
      },
      ...
    ]
    ```
    
    We would prefer to have the endpoint respond dynamically to the request `/api/stats/badges` with the corresponding payload:
    
    ```json
    stats: [
      {
        "id": "1",
        "avatar_url": "http://www.example.org/picture1.jpg",
        "nickname": "octocat1",
        "count": 3
      },
      {
        "id": "2",
        "avatar_url": "http://www.example.org/picture2.jpg",
        "nickname": "octocat2",
        "count": 3
      },
      ...
    ]
    ```
    
    What this does
    - Refactors `Api::StatsController` to query the database based on the value in the `category` parameter.
    - Refactors `StatsSerializer` to no longer inherit from `UserSerializer`.

--- app/controllers/api/stats_controller.rb
+++ app/controllers/api/stats_controller.rb
@@ -3,9 +3,29 @@ module Api
     before_action :authenticate!
 
     def index
-      @users = User.all
+      case params[:category]
+      when "pull_requests"
+        @stats = PullRequest.all.group("user_id").count
+      when "reviews"
+        @stats = PullRequestReview.all.group("user_id").count
+      when "additions"
+        @stats = PullRequest.all.group("user_id").sum(:number_of_additions)
+      when "deletions"
+        @stats = PullRequest.all.group("user_id").sum(:number_of_deletions)
+      when "badges"
+        @stats = Badge.all.group("user_id").count
+      end
 
-      render json: @users, each_serializer: StatsSerializer, root: 'stats'
+      @stats = @stats.map do |key, value|
+        {
+          "id" => key,
+          "avatar_url" => User.find(key).avatar_url,
+          "nickname" => User.find(key).nickname,
+          "count" => value
+        }
+      end
+
+      render json: @stats, each_serializer: StatsSerializer, root: 'stats'
     end
   end
 end
