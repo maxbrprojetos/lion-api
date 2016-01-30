f679a4ea56703837e8d2df53a3f3b05a5444a94d
Kaosisochukwu Uzokwe <kuzokwe@gmail.com>
2016-01-29 20:52:26 -0500

    Remove database_cleaner gem

--- Gemfile
+++ Gemfile
@@ -37,6 +37,5 @@ group :test, :development do
 end
 
 group :test do
-  gem 'database_cleaner'
   gem 'webmock'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -33,7 +33,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = false
+  config.use_transactional_fixtures = true
 
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
@@ -59,19 +59,5 @@ RSpec.configure do |config|
   config.before(:each, type: :controller) do
     allow_any_instance_of(ApplicationController)
       .to receive(:current_user).and_return(current_user)
-  end
-
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-    FactoryGirl.lint
-    DatabaseCleaner.clean_with(:truncation)
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
   end
 end
