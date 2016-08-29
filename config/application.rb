a34366e507e8f963c249e4bbcb989e13a059c40d
Kaosisochukwu Uzokwe <kuzokwe@gmail.com>
2016-08-29 16:17:37 -0400

    Set config.api_only to true
    The name of this app is lion-api, so it makes no sense why we are setting asset config details. Kill with :fire:

--- config/application.rb
+++ config/application.rb
@@ -17,19 +17,9 @@ module Lion
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run 'rake -D time' for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-
     config.active_record.schema_format = :ruby
     # config.active_record.raise_in_transactional_callbacks = true
 
-    config.middleware.use config.session_store, config.session_options
-    config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore
+    config.api_only = true
   end
 end
