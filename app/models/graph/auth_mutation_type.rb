e31741613a4a6fd43ef4f35aa76cdd7e877f5d29
Kaosisochukwu Uzokwe <kuzokwe@gmail.com>
2018-01-23 16:23:24 -0500

    Disable Google Oauth in develoment mode
    https://github.com/alphasights/lion-api/pull/97 introduced seed data to
    aid local development. None of that matter if you cannot log into the
    UI.

--- app/models/graph/auth_mutation_type.rb
+++ app/models/graph/auth_mutation_type.rb
@@ -3,23 +3,27 @@ module Graph
     name "AuthMutation"
     description "The mutation root of this schema"
 
-    field :token_create do
-      type Graph::TokenType
+    field :token_create, Graph::TokenType do
       argument :code, !types.String, "The authorization code for the user"
       resolve -> (object, arguments, context) do
-        app_client = Octokit::Client.new(
-          client_id: ENV['GITHUB_APP_ID'],
-          client_secret: ENV['GITHUB_APP_SECRET']
-        )
-        token = app_client.exchange_code_for_token(arguments[:code])
-        user_client = Octokit::Client.new(access_token: token.access_token)
-        user_data = user_client.user
+        user = if ENV['DISABLE_GOOGLE_AUTH'] == "true"
+          User.first
+        else
+          app_client = Octokit::Client.new(
+            client_id: ENV['GITHUB_APP_ID'],
+            client_secret: ENV['GITHUB_APP_SECRET']
+          )
+          token = app_client.exchange_code_for_token(arguments[:code])
+          user_client = Octokit::Client.new(access_token: token.access_token)
 
-        if user_data.email.blank?
-          user_data.email = user_client.emails.find(&:primary).email
+          user_data = user_client.user
+
+          if user_data.email.blank?
+            user_data.email = user_client.emails.find(&:primary).email
+          end
+
+          User.find_or_create_from_oauth(user_data, token.access_token)
         end
-
-        user = User.find_or_create_from_oauth(user_data, token.access_token)
 
         if user.present? && user.persisted?
           user.access_tokens.active.first_or_create
